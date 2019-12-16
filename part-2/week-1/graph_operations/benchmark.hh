6101d5e8f439e4d75cf44acb14eedf8f2737fa13
Tanu Rana <2907.tanu@gmail.com>
2019-12-16 19:32:12 +0530

    Week-1-exercises

---
+++ part-2/week-1/graph_operations/benchmark.hh
@@ -0,0 +1,10 @@
+ * Contains declarations for function to calculate time difference between
+ * two timeval structures.
+ */
+
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+double calculate(const struct rusage *b, const struct rusage *a);
+
+#endif
