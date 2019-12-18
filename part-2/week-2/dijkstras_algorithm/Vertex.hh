1215a8d3c19b08e80d8365c613af66469ad20897
Tanu Rana <2907.tanu@gmail.com>
2019-12-18 19:32:12 +0530

    Week-2-exercises

---
+++ part-2/week-2/dijkstras_algorithm/Vertex.hh
@@ -0,0 +1,22 @@
+ * Represents a vertex of a graph that is connected to other vertices which
+ * are represented in the form of a list of edges.
+ */
+
+#ifndef VERTEX_HH
+#define VERTEX_HH
+
+#include <list>
+
+#include "Edge.hh"
+
+using namespace std;
+
+struct Vertex {
+    bool is_visited;
+    list<Edge> edges;
+
+    Vertex(bool is_visited = false);
+    ~Vertex(void);
+};
+
+#endif
---
+++ part-2/week-2/dijkstras_algorithm/helpers.hh
@@ -0,0 +1,12 @@
+ * Contains helper/utility functions for performing mundane tasks like
+ * loading a graph from a file.
+ */
+
+#ifndef HELPERS_HH
+#define HELPERS_HH
+
+#include "Graph.hh"
+
+void load_graph_from_file(Graph &G, int n, char *file_name);
+
+#endif
