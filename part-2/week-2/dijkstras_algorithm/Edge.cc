4443ce19b096cd63e8c5c0d20ccfe0ec42b1de1e
Tanu Rana <2907.tanu@gmail.com>
2019-12-20 19:32:12 +0530

    delete redundant files

--- part-2/week-2/dijkstras_algorithm/Edge.cc
+++
@@ -1,7 +0,0 @@
-#include "Edge.hh"
-
-Edge::Edge(int head, int weight)
-{
-    this->head   = head;
-    this->weight = weight;
-}
--- part-2/week-4/2sum/benchmark.h
+++
@@ -1,8 +0,0 @@
-#ifndef BENCHMARK_H
-#define BENCHMARK_H
-
-#include <sys/resource.h>
-
-double calculate(const struct rusage *b, const struct rusage *a);
-
-#endif
