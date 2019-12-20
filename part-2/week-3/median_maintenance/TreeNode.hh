3ec44af568ed264dccf72dcd6d1d9516d6cec262
Tanu Rana <2907.tanu@gmail.com>
2019-12-20 19:32:12 +0530

    Week-3-exercises

---
+++ part-2/week-3/median_maintenance/TreeNode.hh
@@ -0,0 +1,19 @@
+/**
+ * Represents a single node of a tree.
+ */
+
+#ifndef TREENODE_HH
+#define TREENODE_HH
+
+struct TreeNode {
+    int data;
+    int nodes; /* Represents the number of nodes in the subtree for which this
+                  node is the root node */
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int data);
+    ~TreeNode(void);
+};
+
+#endif
